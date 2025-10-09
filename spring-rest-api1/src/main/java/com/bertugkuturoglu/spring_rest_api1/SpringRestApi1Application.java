18942d6d37f8cda18005194aa01eab7d6c63aaf3
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-10-09 18:11:52 +0300

    DenemeCommiti

---
+++ spring-rest-api1/src/main/java/com/bertugkuturoglu/spring_rest_api1/SpringRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.bertugkuturoglu.spring_rest_api1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApi1Application.class, args);
+	}
+
+}
---
+++ spring-rest-api1/src/test/java/com/bertugkuturoglu/spring_rest_api1/SpringRestApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertugkuturoglu.spring_rest_api1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
