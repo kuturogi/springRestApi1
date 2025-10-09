dae72596d3fd15e2d91ce7ff4b7f82dd60fd63f7
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-10-09 23:51:16 +0300

    Create EmployeeService.java

---
+++ src/main/java/com/bertugkuturoglu/services/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.bertugkuturoglu.services;
+
+import com.bertugkuturoglu.model.Employee;
+import com.bertugkuturoglu.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    public List<Employee> getAllEmployeeList() {
+        return employeeRepository.GetAllEmployeelist();
+
+    }
+}
